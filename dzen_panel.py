39a5b302f79a4c93f0b857279c310703e21d0ce4
damicore <damiangaggero@gmail.com>
2014-12-11 14:09:56 -0300

    Modified dzen_panel.py

--- dzen_panel.py
+++ dzen_panel.py
@@ -1,22 +1,58 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python
 
+import sys
+import string
 import os
 
 status_cmd = 'herbstclient tag_status'
+idle_cmd = 'herbstclient --idle'
 
-def parse_tags(status):
+def parse_tags(output):
     #Takes herbstclient tag_status and parses it
     #returns a nested list of tags.
-    tags = status.split()
+    tags = output.split()
+    taglist = ''
     for i in range(len(tags)):
-        tag = tags[i]
-        t = list(tag)
-        tags[i] = t
-    return tags
+        if tags[i][0] == '#':
+            s = '^bg(' + colors[3] + ')' + tags[i][1:] + ' '
+            taglist += s
+        elif tags[i][0] == ':':
+            taglist += '^bg(' + colors[4] + ')' + tags[i][1:] + ' '
+    return taglist
 
-while True:
-    proc = os.popen(status_cmd)
-    output = proc.readline()
-    tag_list = parse_tags(output)
-    print tag_list
+def parse_xdef():
+    os.chdir('/home/damian/')
+    fin = open('.Xresources')
+    colors = []
+    for line in fin:
+        color = line.strip()
+        if 'color' in color:
+            rgb = color[color.find('#'):]
+            colors.append(rgb)
+        elif 'background' in color:
+            colors.append(color[color.find('#'):])
+        elif 'foreground' in color:
+            colors.append(color[color.find('#'):])
+    return(colors)
 
+colors = parse_xdef()
+single_line_out = ''
+idlein = os.popen(idle_cmd)
+#for line in idlein:
+#while True:
+line = idlein.readline()
+    #if 'tag' in line:
+proc = os.popen(status_cmd)
+output = proc.readline()
+taglist = parse_tags(output)
+single_line_out = taglist + ' hola'
+sys.stdout.write(single_line_out)
+
+#while True:
+    #idlein = os.popen(idle_cmd)
+    #print(idlein.readline())
+    #if 'tag' in event:
+    #proc = os.popen(status_cmd)
+    #output = proc.readline()
+    #taglist = parse_tags(output)
+    #print(taglist)
